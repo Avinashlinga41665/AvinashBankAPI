c614e9af718c7302803a2e91c5fe23f61f196864
AvinashLinga <avinashlinga41665@gmail.com>
2025-06-01 22:42:41 +0530

    Initial commit of ASP.NET backend project

---
+++ Models/Login.cs
@@ -0,0 +1,12 @@
+﻿namespace AvinashBankBackEnd.Models
+{
+    public partial class Login
+    {
+        
+        public string Userloginname { get; set; }
+
+        public string Userloginpwd { get; set; }
+
+
+    }
+}
